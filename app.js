c2b861f00f49fe453eed5a49d8c9b082a9ac6caf
Erik Granlund <grer18qp@student.ju.se>
2019-09-06 14:50:46 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const dummyData = require('./dummy-data')
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+app.engine("hbs", expressHandlebars({
+  defaultLayout: 'main.hbs'
+}))
+
+app.get('/', function(request, response){
+  const model = {
+    humans: dummyData.humans
+  }
+  response.render("show-all-humans.hbs", model)
+})
+
+app.get('/main', function(request, response){
+  response.render('./main.hbs')
+})
+
+app.get('/about', function(request, response){
+  response.render('./about.hbs')
+})
+
+app.get('/Contact', function(request, response){
+  response.render('./contact.hbs')
+})
+
+app.get('/main.css', function(request,response){
+response.sendFile(__dirname + '/views/main.css')
+})
+
+app.listen(8080)
