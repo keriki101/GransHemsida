d0b590a8120051a60da33051db1ffb344eaaa0f4
kerki101 <kireerik2008@hotmail.com>
2019-10-18 11:29:46 +0200

    added 404 and 500 page isch

--- app.js
+++ app.js
@@ -109,3 +109,12 @@ app.get('/login', function(request, response){
 
 
 app.listen(8080)
+
+app.get('*', function(req, res){
+  res.send('ERROR 404: Could Not Find', 404);
+});
+
+app.use(function (error, req, res, next) {
+  res.send('500: Internal Server Error', 500);
+  console.log(error);
+});
