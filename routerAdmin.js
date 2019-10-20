c336e000564cd0342128f585867bfba96c0bf83c
Erik G <kiree@5CG5264CD4.learnet.se>
2019-10-20 12:19:48 +0200

    changed some links

--- routerAdmin.js
+++ routerAdmin.js
@@ -4,7 +4,7 @@ const db = require('./db')
 const router = express.Router()
 
 router.get('/', function (request, response) {
-  response.render("adminpage.hbs")
+  response.render("adminPage.hbs")
 })
 
 router.get('/manageguestbook', function (request, response) {
